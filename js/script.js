83a3ae609588e6d7cb8d17933538675305a6773f
Dima Pozharniy <pezh2008@gmail.com>
2023-05-24 23:18:30 +0300

    375px bag

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 new Swiper(".banner", {
   wrapperClass: "banner__wrapper", // для переопределения swiper-wrapper на свой класс
-  slideClass: "banner__slide",//для переопределения swiper-slide на свой класс
+  slideClass: "banner__slide", //для переопределения swiper-slide на свой класс
   // Optional parameters
 
   // If we need pagination
   pagination: {
     el: ".banner__pagination", //для переопределения swiper-pagination на свой класс
-    clickable: true
+    clickable: true,
   },
 
   // Navigation arrows
@@ -15,4 +15,37 @@ new Swiper(".banner", {
     prevEl: ".banner__prev",
   },
 });
-console.dir(Swiper);
+
+new Swiper(".stock-slider", {
+  wrapperClass: "stock-slider__wrapper",
+  slideClass: "stock-slider__slide",
+  spaceBetween: 30, // Растояние между сладерами
+  slidesPerView: 3, // Количество отображаемых слайдов
+
+  pagination: {
+    el: ".stock-slider__pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".stock-slider__next",
+    prevEl: ".stock-slider__prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    375: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    980: {
+      slidesPerView: 3,
+    },
+  },
+});
